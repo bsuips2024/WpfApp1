b5146246a07e5e931eacc8a1b3e921bcc1b4c314
Преподаватель <@M15-918>
2024-04-13 12:59:11 +0300

    я много чего добавил по сравнению с прошлым проектом, только я удалил

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,33 +16,37 @@ using System.Windows.Shapes;
 
 namespace WpfApp1
 {
+
+    public class Phone
+    {
+        public string Title { get; set; }
+        public string Company { get; set; }
+        public int Price { get; set; }
+    }
+
+    public class Person
+    {
+        public int ID { get; set; }
+        public string NAME { get; set; }
+    }
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        int res = 0;
+        string s = "√";
+
+
         public MainWindow()
         {
-            
-               
-                InitializeComponent();
-                label.Content = "Привет";
+
+
+            InitializeComponent();
+
 
         }
 
-        private void btn_Click(object sender, RoutedEventArgs e)
-        {
-    try
-    {
-        int zerro = 0;
-            int a = 2/ zerro;
-            MessageBox.Show(Convert.ToString(a));
 
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
     }
 }
